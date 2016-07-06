7c04068a4fbc26ab81e0694a9f993b78ad9241a8
doug-kretz <doug.kretz@gmail.com>
2016-07-05 18:37:19 -0700

    Use App.js to render everything. Completed Props tutorial

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render(){
-		return <div>Hello</div>;
+	let txt = this.props.txt;
+	return <h1>{txt}</h1>
 	}
 }
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this i sthe default txt'
+}
+
+ReactDOM.render(
+	<App txt="what" cat={5} />,
+	document.getElementById('app')
+	);
 
 export default App
